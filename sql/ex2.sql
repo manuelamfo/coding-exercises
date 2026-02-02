4bd87a5f7afe5386f7ec14fe1c4052b4315a0553
manuela <98962707+manuelamfo@users.noreply.github.com>
2026-02-02 13:28:33 -0300

    Create ex2.sql

---
+++ sql/ex2.sql
@@ -0,0 +1,5 @@
+/*
+HackerRank - Japanese Cities' Names
+Query the names of all the Japanese cities in the CITY table. The COUNTRYCODE for Japan is JPN. 
+*/
+SELECT name FROM CITY WHERE countrycode = 'JPN';
